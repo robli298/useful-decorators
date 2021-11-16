aaebfed3841b5bcd0ddf6252efa0f51ae804ce9c
Jana Liebke <robsonliebke@gmail.com>
2021-11-17 00:17:52 +0100

    add default options

--- src/sort-by.test.ts
+++ src/sort-by.test.ts
@@ -6,7 +6,9 @@ interface Item {
 }
 
 export class Greeting {
-    @SortBy('key')
+    @SortBy('key', {
+        isAscending: false
+    })
     array: Item[] = [{
         key: 3,
         value: 'a'
@@ -18,8 +20,13 @@ export class Greeting {
         value: 'c'
     }]
 
-    @SortBy('key')
-    name: string = 'test';
+    @SortBy('', {
+        isAscending: false
+    })
+    public numbers: number[] = [3, 4, 1, 9];
+
+    @SortBy()
+    public dates: Date[] = [new Date('2020-06-17'), new Date('2020-06-16'), new Date('2020-06-20'), new Date('2020-06-10')];
 
     constructor() { }
 
@@ -28,7 +35,7 @@ export class Greeting {
     }
 }
 
-it('It should sort the array', () => {
+xit('It should sort the array of object by key', () => {
     expect([{
         key: 1,
         value: 'c'
@@ -39,4 +46,24 @@ it('It should sort the array', () => {
         key: 3,
         value: 'a'
     }]).toEqual(new Greeting().array);
+})
+
+it('It should sort the array numbers in descending order', () => {
+    expect([9, 4, 3, 1]).toEqual(new Greeting().numbers);
+})
+
+xit('It should sort date array', () => {
+    const dates = [new Date('2020-06-10'), new Date('2020-06-16'), new Date('2020-06-17'), new Date('2020-06-20')];
+    expect(dates).toEqual(new Greeting().dates);
+})
+
+xit('It should thrown an error', () => {
+    expect(() => {
+        class Foo {
+            @SortBy()
+            myNumber: number = 1;
+            constructor() { }
+        }
+        new Foo();
+    }).toThrow(Error)
 })
--- src/sort-by.ts
+++ src/sort-by.ts
@@ -1,21 +1,43 @@
-export function SortBy<T extends Array<any>>(property: string) {
+// TODO check date sorting
+// TODO check string sorting
+
+const sortFn = (p: string | symbol | number | undefined, isAscending: boolean) => {
+    if (isAscending) {
+        return (a: any, b: any) => {
+            if (p && a.hasOwnProperty(p) && b.hasOwnProperty(p)) {
+                return a[p] - b[p];
+            }
+            return a - b;
+        }
+    } else {
+        return (a: any, b: any) => {
+            if (p && b.hasOwnProperty(p) && a.hasOwnProperty(p)) {
+                return b[p] - a[p];
+            }
+            return b - a;
+        }
+    }
+}
+
+export function SortBy<T>(sortByProperty?: string | symbol | number, options?: { isAscending?: boolean, type?: 'string' | 'number' | 'date' | 'object' }) {
+    const defaultOptions = {
+        ...{
+            isAscending: true,
+            type: 'object'
+        }, ...options
+    }
+    const value = Symbol();
+
     return function (target: any, propertyKey: string) {
-
-        let value: T;
-
-        const getter = function () {
-            return value;
-        }
-
-        const setter = function (newValue: T) {
-            value = newValue.sort((a: any, b: any) => {
-                return a[property] - b[property];
-            })
-        }
-
         Object.defineProperty(target, propertyKey, {
-            set: setter,
-            get: getter
+            set: (newValue: Array<T>) => {
+                if (!newValue || !Array.isArray(newValue)) {
+                    throw Error(`The ${propertyKey} is not an Array!`)
+                }
+                target[value] = newValue.sort(sortFn(sortByProperty, defaultOptions.isAscending));
+            }
+            ,
+            get: () => target[value]
         })
     }
 }
