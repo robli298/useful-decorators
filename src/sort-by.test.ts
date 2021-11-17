1f0bd34c687a7194dc89cf2b82ca47b47fe4e7ba
Jana Liebke <robsonliebke@gmail.com>
2021-11-18 00:57:30 +0100

    clean up #1

--- src/sort-by.test.ts
+++ src/sort-by.test.ts
@@ -6,24 +6,31 @@ interface Item {
 }
 
 export class Greeting {
-    @SortBy('key', {
-        isAscending: false
+    @SortBy('value', {
+        isAscending: false,
+        type: 'string'
     })
     array: Item[] = [{
-        key: 3,
+        key: 1,
         value: 'a'
     }, {
         key: 2,
         value: 'b'
     }, {
-        key: 1,
+        key: 3,
         value: 'c'
     }]
 
     @SortBy('', {
-        isAscending: false
+        isAscending: false,
+        type: 'number'
     })
     public numbers: number[] = [3, 4, 1, 9];
+
+    @SortBy('', {
+        type: 'number'
+    })
+    public numbersAscending: number[] = [4, 3, 2, 1];
 
     @SortBy()
     public dates: Date[] = [new Date('2020-06-17'), new Date('2020-06-16'), new Date('2020-06-20'), new Date('2020-06-10')];
@@ -35,15 +42,15 @@ export class Greeting {
     }
 }
 
-xit('It should sort the array of object by key', () => {
+it('It should sort the array of object by key', () => {
     expect([{
-        key: 1,
+        key: 3,
         value: 'c'
     }, {
         key: 2,
         value: 'b'
     }, {
-        key: 3,
+        key: 1,
         value: 'a'
     }]).toEqual(new Greeting().array);
 })
@@ -52,12 +59,16 @@ it('It should sort the array numbers in descending order', () => {
     expect([9, 4, 3, 1]).toEqual(new Greeting().numbers);
 })
 
-xit('It should sort date array', () => {
+it('It should sort the array numbers in ascending order', () => {
+    expect([1, 2, 3, 4]).toEqual(new Greeting().numbersAscending);
+})
+
+it('It should sort date array', () => {
     const dates = [new Date('2020-06-10'), new Date('2020-06-16'), new Date('2020-06-17'), new Date('2020-06-20')];
     expect(dates).toEqual(new Greeting().dates);
 })
 
-xit('It should thrown an error', () => {
+it('It should thrown an error', () => {
     expect(() => {
         class Foo {
             @SortBy()
--- src/sort-by.ts
+++ src/sort-by.ts
@@ -1,43 +1,52 @@
-// TODO check date sorting
-// TODO check string sorting
 
-const sortFn = (p: string | symbol | number | undefined, isAscending: boolean) => {
-    if (isAscending) {
+
+const sortFunctionFactory = (isAscending: boolean | undefined, sortByProperty: string | symbol | number | undefined) => {
+    const inversor = isAscending ? 1 : -1;
+    if (sortByProperty) {
         return (a: any, b: any) => {
-            if (p && a.hasOwnProperty(p) && b.hasOwnProperty(p)) {
-                return a[p] - b[p];
+            if (a[sortByProperty] < b[sortByProperty]) {
+                return -1 * inversor;
             }
-            return a - b;
-        }
-    } else {
-        return (a: any, b: any) => {
-            if (p && b.hasOwnProperty(p) && a.hasOwnProperty(p)) {
-                return b[p] - a[p];
+
+            if (a[sortByProperty] > b[sortByProperty]) {
+                return 1 * inversor;
             }
-            return b - a;
+
+            return 0;
         }
     }
-}
+    return (a: any, b: any) => {
+        if (a < b) {
+            return -1 * inversor;
+        }
 
-export function SortBy<T>(sortByProperty?: string | symbol | number, options?: { isAscending?: boolean, type?: 'string' | 'number' | 'date' | 'object' }) {
-    const defaultOptions = {
-        ...{
-            isAscending: true,
-            type: 'object'
-        }, ...options
+        if (a > b) {
+            return 1 * inversor;
+        }
+
+        return 0;
     }
+};
+
+export function SortBy<T>(sortByProperty?: string | symbol | number, options?: { isAscending?: boolean, type: 'string' | 'number' | 'date' }) {
+
+    options = { ...{ type: 'string', isAscending: true }, ...options };
+
     const value = Symbol();
 
     return function (target: any, propertyKey: string) {
         Object.defineProperty(target, propertyKey, {
-            set: (newValue: Array<T>) => {
+            set: function (newValue: Array<T>) {
                 if (!newValue || !Array.isArray(newValue)) {
                     throw Error(`The ${propertyKey} is not an Array!`)
                 }
-                target[value] = newValue.sort(sortFn(sortByProperty, defaultOptions.isAscending));
+                target[value] = newValue.sort(
+                    sortFunctionFactory(options?.isAscending, sortByProperty)
+                );
             }
             ,
-            get: () => target[value]
+            get: function () { return target[value]; }
         })
     }
 }
+
