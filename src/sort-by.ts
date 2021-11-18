9c32088915cc61d4ebc8828e59f5d8dbd7c461e8
Jana Liebke <robsonliebke@gmail.com>
2021-11-19 00:34:31 +0100

    clean up #3

--- src/sort-by.ts
+++ src/sort-by.ts
@@ -2,23 +2,23 @@ const sortFunctionFactory = (isAscending: boolean | undefined, type?: 'string' |
     const order = isAscending ? 1 : -1;
     if (sortByProperty) {
         return (a: any, b: any) => {
+            let sortResult = 0;
             if (a[sortByProperty] < b[sortByProperty]) {
-                return -1 * order;
+                sortResult = -1;
+            } else if (a[sortByProperty] > b[sortByProperty]) {
+                sortResult = 1;
             }
-            if (a[sortByProperty] > b[sortByProperty]) {
-                return 1 * order;
-            }
-            return 0;
+            return sortResult * order;
         }
     }
     return type === 'string' ? sortFunctionStringFactory(order, sortByProperty) : (a: any, b: any) => {
+        let sortResult = 0;
         if (a < b) {
-            return -1 * order;
+            sortResult = -1;
+        } else if (a > b) {
+            sortResult = 1;
         }
-        if (a > b) {
-            return 1 * order;
-        }
-        return 0;
+        return sortResult * order;
     }
 };
 
