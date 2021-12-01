c390aff53590f1f3d43cd121af9345351e98c879
Robson Liebke <liebke@media-interactive.de>
2021-12-01 23:19:19 +0100

    implement debounce decorator

--- src/_tests/debounce.test.ts
+++ src/_tests/debounce.test.ts
@@ -1,15 +1,56 @@
 import { Debounce } from '../debounce';
 
-it('...', () => {
-	class TestClass {
+jest.mock('lodash/debounce', () => {
+	return (func: any, wait: number) => {
+		let timeout: any;
+		return function (this: any, ...args: any[]) {
+			const context = this;
+			clearTimeout(timeout);
+			timeout = setTimeout(() => func.apply(context, args), wait);
+		};
+	};
+});
+
+jest.useFakeTimers();
+
+describe('debounce', () => {
+	class Greeting {
+		i = 0;
+		constructor() {
+			// not yet implemented
+		}
 		@Debounce(100)
-		greet(a: string) {
-			console.log(`Hello ${a}!`);
+		increment(value: number) {
+			this.i++;
+			this.i+=(+value);
 		}
 	}
 
-	const t = new TestClass();
-	t.greet('Robs');
+	test('should increment once', () => {
+		const g = new Greeting();
+		g.increment(0);
+		g.increment(0);
+		g.increment(0);
+		jest.advanceTimersByTime(200);
 
-	expect(true).toEqual(true);
+		expect(g.i).toEqual(1);
+	});
+
+	test('should increment twice', () => {
+		const g = new Greeting();
+		g.increment(0);
+		jest.advanceTimersByTime(200);
+		g.increment(0);
+
+		expect(g.i).toEqual(1);
+	});
+
+	test('should increment once and add value given', () => {
+		const g = new Greeting();
+		g.increment(2);
+		g.increment(2);
+		jest.advanceTimersByTime(200);
+
+		expect(g.i).toEqual(3);
+	});
 });
